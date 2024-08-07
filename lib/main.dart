0171b54147980e1eec7f00a5d36ec04e0ed22589
ridhofajriawan16 <ridhofajriawan16@gmail.com>
2024-08-07 04:21:15 +0000

    fluter api

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:myapp/auth/login_page.dart';
+import 'package:myapp/auth/register_page.dart';
+import 'package:myapp/form/counter_page.dart';
+import 'package:myapp/form/display_page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: LoginPage(),
+    );
+  }
+}
+
+class BelajarColumn extends StatelessWidget {
+  const BelajarColumn({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Persegi(),
+          // PersegiPanjang(),
+          // Segitiga(),
+          // Lingkaran(),
+          // Kerucut(),
+        ]
+      ),
+    );
+  }
+}
